bacfb632ca1668e927b9da361f412899f7366059
raven <78323525+raavenkkj@users.noreply.github.com>
2023-08-16 19:19:54 -0300

    NEW KEY SOURCE

--- yuto-hub.lua
+++ yuto-hub.lua
@@ -5,7 +5,7 @@ lib.Webhook = "https://canary.discord.com/api/webhooks/1099027131121532979/UkUkq
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Binintrozza/yutv2e/main/afss"))()
 wait()
 local StarterGui = game:GetService("StarterGui")
-local url = "https://s3.eu-central-1.amazonaws.com/adlocis.linkvertise.links/pastes/143222647.txt?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA6L5L3NKTBHJ3YVHU%2F20230811%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20230811T072611Z&X-Amz-SignedHeaders=host&X-Amz-Expires=432000&X-Amz-Signature=9341330e4573d1644840d78baaf7c58d6f3b09515eecfa1586000ebda17b2721"
+local url = "https://s3.eu-central-1.amazonaws.com/adlocis.linkvertise.links/pastes/143222647.txt?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA6L5L3NKTBHJ3YVHU%2F20230816%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20230816T065005Z&X-Amz-SignedHeaders=host&X-Amz-Expires=432000&X-Amz-Signature=b164399982226454f53c7a241a06be155f7ec737e2b1a08e6f4c69f2961b2c59"
 local response = request({Url=url, Method="GET"})
 
 if response.StatusCode == 200 then
